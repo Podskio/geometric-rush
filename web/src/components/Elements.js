4775898065431e1c3736dea0b4fdabd01d280255
gavindg <52291693+gavindg@users.noreply.github.com>
2023-11-04 16:42:03 -0700

    Add collision detection for spikes

--- web/src/components/Elements.js
+++ web/src/components/Elements.js
@@ -1,15 +1,27 @@
 import { Bodies, World } from "matter-js";
 
 export function Spike(world, x, y) {
-  const spike = Bodies.fromVertices(x, y, [[{ x: 0, y: 0}, { x: 43, y: 0 }, { x: 21.5, y: -75 }]], {
-    mass: 10,
-    restitution: 0,
-    friction: 0.005,
-    isStatic: true,
-    render: {
-      fillStyle: "#D9D9D9",
-    },
-  });
+  const spike = Bodies.fromVertices(
+    x,
+    y,
+    [
+      [
+        { x: 0, y: 0 },
+        { x: 43, y: 0 },
+        { x: 21.5, y: -75 },
+      ],
+    ],
+    {
+      mass: 10,
+      restitution: 0,
+      friction: 0.005,
+      isStatic: true,
+      render: {
+        fillStyle: "#D9D9D9",
+      },
+      name: "spike",
+    }
+  );
 
   World.add(world, [spike]); // add new objects to this array.
 }
