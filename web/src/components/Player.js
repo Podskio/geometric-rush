f381ed626c89be1c30695a1e46d142c943dc291d
Podskio <podskio1@gmail.com>
2023-11-04 16:57:27 -0700

    Revert "Add collision detection for spikes"
    This reverts commit 4775898065431e1c3736dea0b4fdabd01d280255.

--- web/src/components/Player.js
+++ web/src/components/Player.js
@@ -1,60 +1,48 @@
 import { Events, World, Body, Bodies } from "matter-js";
 
-function Player(canMove, engine, runner) {
-  //   let time = 0;
+function Player(canMove, engine, runner){
+    
+    let time = 0;
+    
+    const player = Bodies.rectangle(160, 300, 45, 45, 
+        {mass: 10, friction: 0.1, render: {fillStyle: "#ff6262"}})
 
-  const player = Bodies.rectangle(160, 300, 45, 45, {
-    mass: 10,
-    friction: 0.1,
-    render: { fillStyle: "#ff6262" },
-    name: "player",
-  });
+    const keys = {};
+    
+    document.addEventListener('keydown', (event) => {
+        keys[event.key] = true;
+        const force = 0.05;
 
-  const keys = {};
+        if(event.key == 'w' || event.key == 'ArrowUp'){
+            Body.applyForce(player, player.position, { x: 0, y: -force*2})
+        }
+            
+    });
+    
+    document.addEventListener('keyup', (event) => {
+        keys[event.key] = false;
+        
+    });
+    
 
-  document.addEventListener("keydown", (event) => {
-    keys[event.key] = true;
-    const force = 0.05;
+    Events.on(runner, "beforeUpdate", () => {
+        const force = 0.0025;
+        
 
-    if (event.key == "w" || event.key == "ArrowUp") {
-      Body.applyForce(player, player.position, { x: 0, y: -force * 2 });
-    }
-  });
+        if (keys['a'] || keys['ArrowLeft']){
+            Body.applyForce(player, player.position, { x: -force, y: 0})
+        }
 
-  document.addEventListener("keyup", (event) => {
-    keys[event.key] = false;
-  });
+        if (keys['d'] || keys['ArrowRight']){
+            Body.applyForce(player, player.position, { x: force, y: 0})
+        }
+    })
 
-  Events.on(runner, "beforeUpdate", () => {
-    const force = 0.0025;
 
-    if (keys["a"] || keys["ArrowLeft"]) {
-      Body.applyForce(player, player.position, { x: -force, y: 0 });
-    }
+    
 
-    if (keys["d"] || keys["ArrowRight"]) {
-      Body.applyForce(player, player.position, { x: force, y: 0 });
-    }
-  });
-
-  Events.on(engine.current, "collisionStart", (event) => {
-    const pairs = event.pairs;
-    // console.log(pairs); TODO: remove me!
-
-    for (const pair of pairs) {
-      //   console.log(pair.bodyA.name + " collided with " + pair.bodyB.name); TODO: remove me!
-      const bodyA = pair.bodyA;
-      const bodyB = pair.bodyB;
-      if (
-        (bodyA.name === "player" && bodyB.name === "spike") ||
-        (bodyA.name === "spike" && bodyB.name == "player")
-      ) {
-        console.log("player dead!");
-      }
-    }
-  });
-
-  World.add(engine.current.world, [player]);
+    World.add(engine.current.world, [player]);
 }
+
 
 export default Player;
